e74985bc115837bbe817b668f3084f3d783ea344
Ranajit Mane <maneranajit@gmail.com>
2018-02-06 01:04:00 +0530

    style added in Home page

---
+++ src/components/Home.vue
@@ -0,0 +1,301 @@
+<template>
+<div>
+<section style="padding-top:0">
+  <div class="splash container-fluid">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">		
+          <h1 class="animated fadeInUp"> Brand Simply </h1>
+          <h2 class="animated fadeInDown">Marketing Product Solutions</h2>
+       	</div>
+        <div class="section-link col-xs-4 col-xs-offset-2 col-md-2 col-md-offset-2">
+          <a class="circle products" data-target="#products"><i class="fa fa-shopping-basket fa-3x"></i></a>
+          <h3 class="animated">Products</h3>
+        </div>
+        <div class="section-link col-xs-4 col-md-2">
+			<a class="circle platform" data-target="#platform"><i class="fa fa-laptop fa-3x"></i></a>
+       		<h3 class="animated">Platform</h3>
+        </div>
+        <div class="section-link col-xs-4 col-xs-offset-2 col-md-2 col-md-offset-0">
+          <a class="circle logistics" data-target="#logistics"><i class="fa fa-arrow-right fa-3x"></i></a>
+          <h3 class="animated">Logistics</h3>
+        </div>
+        <div class="section-link col-xs-4 col-md-2">
+          <a class="circle management" data-target="#management"><i class="fa fa-cogs fa-3x"></i></a>
+          <h3 class="animated">Management</h3>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <div class="banner container-fluid">
+    <div class="quote container">
+      <h3>"yurstore turned an all day project into an easy 15 minute solution!"</h3>
+      <h4> - customer name</h4>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="under-banner container-fluid">
+    <div class="container">
+      <h1>Start building your store! &nbsp; <i class="fa fa-arrow-circle-right fa-lg"></i></h1>
+    </div>
+  </div>
+</section>
+
+<section id="products" class="dark">
+  <div class="container text-center">
+    <div class="col-md-8 col-sm-9">
+      <div class="description">
+        <h1>Products</h1>
+        <hr>
+        <p> Through consultation we offer the products your employees need to reinforce your brand and get the job done in comfort and style.
+        </p>
+      </div>
+    </div>
+    <div class="col-md-4 col-sm-3">
+      <div class="image">
+        <div class="inset">
+          <img src="https://image.freepik.com/free-vector/identity-corporate-for-sea-food-restaurant_23-2147560164.jpg">
+        </div>
+      </div>
+    </div>
+</div>
+</section>
+
+<section id="platform" class="light">
+  <div class="container text-center">
+    <div class="col-md-4 col-sm-3">
+      <div class="image">
+        <div class="inset">
+        <img src="https://image.freepik.com/free-vector/cloud-computing_459-109.jpg">
+        </div>
+      </div>
+    </div>
+    <div class="col-md-8 col-sm-9">
+      <div class="description">
+        <h1>Platform</h1>
+        <hr>
+        <p> Fully integrated platform to present, inform and finalize product purchases. From virtuals to pricing, fitting, timeframe and customer service after purchase.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="logistics" class="dark">
+  <div class="container text-center">
+    <div class="col-md-8 col-sm-9">
+      <div class="description">
+        <h1>Logistics</h1>
+        <hr>
+        <p> Vertically integrated from product presentation, production, decoration to the delivery of the final product in the necessary time frame.
+        </p>
+      </div>
+    </div>
+    <div class="col-md-4 col-sm-3">
+      <div class="image">
+        <div class="inset">
+        <img src="https://image.freepik.com/free-vector/logistics-service-infographic_72147510828.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="management" class="light">
+  <div class="container text-center">
+    <div class="col-md-4 col-sm-3">
+      <div class="image">
+        <div class="inset">
+        <img src="https://image.freepik.com/free-vector/work-process-designs-set_1209-13.jpg">
+        </div>
+      </div>
+    </div>
+    <div class="col-md-8 col-sm-9">
+      <div class="description">
+        <h1>Management</h1>
+        <hr>
+        <p> Providing the necessary information for both administrators and end users to have the knowledge to know all aspects of the order.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section>
+  <div class="container-fluid color-block">
+    <div class="container">
+      <div class="row">
+        <h1>Why use yurstore.com?</h1>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <h2>Simple</h2>
+          <p>Nullam non ex felis. Vivamus at nisi consequat, pretium dolor ut, cursus nulla. Praesent non efficitur est. Mauris a tincidunt nunc.</p>
+        </div>
+        <div class="col-md-3">
+          <h2>Affordable</h2>
+          <p>Nullam non ex felis. Vivamus at nisi consequat, pretium dolor ut, cursus nulla. Praesent non efficitur est. Mauris a tincidunt nunc.</p>
+        </div>
+        <div class="col-md-3">
+          <h2>Efficient</h2>
+          <p>Nullam non ex felis. Vivamus at nisi consequat, pretium dolor ut, cursus nulla. Praesent non efficitur est. Mauris a tincidunt nunc.</p>
+        </div>
+        <div class="col-md-3">
+          <h2>Consistent</h2>
+          <p>Nullam non ex felis. Vivamus at nisi consequat, pretium dolor ut, cursus nulla. Praesent non efficitur est. Mauris a tincidunt nunc.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section>
+  <div class="container">
+    <div class="row text-center">
+      <h1>Who uses yurstore?</h1>
+    </div>
+    <div class="row">
+      <div class="col-sm-4 col-xs-6 client-logo"><img src="http://img.yurstore.ekbana.info/client-logos/fcs-logo.png"></div>
+      <div class="col-sm-4 col-xs-6 client-logo"><img src="http://img.yurstore.ekbana.info/client-logos/lennar-logo.png"></div>
+      <div class="col-sm-4 col-xs-6 client-logo"><img src="http://img.yurstore.ekbana.info/client-logos/gannett-logo.png"></div>
+      <div class="col-sm-4 col-xs-6 client-logo"><img src="http://img.yurstore.ekbana.info/client-logos/21onc-logo.png"></div>
+      <div class="col-sm-4 col-xs-6 client-logo"><img src="http://img.yurstore.ekbana.info/client-logos/pultegroup-logo.png"></div>
+      <div class="col-sm-4 col-xs-6 client-logo"><img src="http://img.yurstore.ekbana.info/client-logos/fgcu-logo.png"></div>
+    </div>
+  </div>
+</section>
+
+</div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Home Page'
+    }
+  }
+}
+</script>
+<style>
+	body { font-family: 'Roboto', sans-serif; font-weight:300; }
+section { padding: 3em 0; }
+section:first-child { padding-top:0; }
+h1 { text-align:center; }
+nav.navbar { background:none; padding:0; border-bottom:none; }
+.navbar-logo { margin:2em 0; height:4em; }
+input#login-field { display:none; }
+.navbar-default .btn-link { height:100%; padding:2em 1em; font-size:1.25em; font-weight:300; color:#eee;  }
+.navbar-default .btn-link:hover { text-decoration:none; color:#ff0; bckground:#f3f3f3;  }
+
+
+.splash { margintop:2em; min-height:85vh; padding:2em; display:flex; align-items: center; background: url("http://murraytweetindex.ie/wp-content/themes/murray-tweet-index/assets/images/bg-header-opt-@2x.png") top center no-repeat,linear-gradient(to right, #39bdcd, #00a2d1 48%, #005872); color:#eee; }
+.splash .container { display:flex; justify-content: space-between; align-items: center; flex-wrap: wrap; text-align:center; }
+.splash .container h1 { font-size:6em; font-family: 'Roboto', sans-serif; font-weight:300; 
+  text-shadow:0 .025em 0 #999,0 .0625em .125em #123; }
+.splash .container h2 { font-size:3em; font-family: 'Roboto', sans-serif; font-weight:300; text-shadow:0 .0625em .125em #123; }
+.splash .container>div { flex:1; width:30%;}
+.splash .container>div ul { padding:0 1em; border-left:solid 1px #eee; list-style:none; }
+
+
+.banner { padding:.5em 0; background:#ccc; boxshadow:0 .25em 1em .25em #456; text-align:center; line-height:1em; }
+.banner h3 { margin:.5em; font-weight:200; color:#666; }
+.banner h4 { margin:.5em; font-weight:200; color:#666; }
+.under-banner { text-align:center; line-height:1em; color:#06C; }
+.under-banner h1 { margin:.5em; font-weight:500; color:#069; }
+
+
+
+.color-block {
+    background: #0077cc;
+    background-image: radial-gradient(circle at bottom center,#0099cc,#1166bb);
+    padding: 6em;
+  color: white; }
+
+
+
+.client-logo { text-align: center; padding: 1em; height: 6em; vertical-align: middle; }
+.client-logo img { display: inline-block; vertical-align: middle; max-width: 150px; }
+
+.light_section { padding:1em 0; background:#f0f0f0; color:#999; }
+
+footer#footer { margin-top:6em; padding:3em 0; background:#123; color:#eee; border-top:solid 2em #ddd; }
+
+section#copyright { padding:1em 0; background:#333; color:#999; }
+
+.section-link { mar-gin:2em 1em 0;padding:1.5em 1em 0; display:inline-block; width:10%; }
+.section-link .circle { margin:2em 0em 0; padding:2em 2em; font-size:.5em; display:inline-block;  position:relative; height:8em; width:8em; background:#eee; color:#999; border-radius:100%; transition:all .25s; z-index:100; }
+.section-link .circle:hover { color:#069; box-shadow:0 0 0 6px #069; transform:translate(0px,-1em); }
+.section-link h3 { margin-top:0; font-weight:300; opacity:0; z-index:-1; }
+.section-link .circle { color:white; }
+.section-link .circle:hover { color:white; }
+.section-link .circle.products { background:#70bf54; }
+.section-link .circle.products:hover { background:#70bf54; box-shadow:0 0 0 6px white; }
+.section-link .circle.platform { background:#ee3f75; }
+.section-link .circle.platform:hover { background:#ee3f75; box-shadow:0 0 0 6px white; }
+.section-link .circle.logistics { background:#ffd65c; background:radial-gradient(circle farthest-corner at top left,#ffe66c,#f6b432); }
+.section-link .circle.logistics:hover { background:#ffd65c; box-shadow:0 0 0 6px white; background:radial-gradient(circle farthest-corner at bottom center,#ffd65c,#f6b432); }
+.section-link .circle.management { background:#b37ab4; background:radial-gradient(circle farthest-corner at top left,#b37ab4,#993399); }
+.section-link .circle.management:hover { background:#b37ab4;box-shadow:0 0 0 6px white; background:radial-gradient(circle farthest-corner at bottom center,#b37ab4,#993399); }
+
+
+.quote h1,.quote h2,.quote h3,.quote h4,.quote h5,.quote h6 { display:inline-block; vertical-align:middle; line-height:1em; color:#666; }
+.quote h4 { paddingtop:.25em; }
+
+
+section .image { margin:auto; height:18em; width:18em; border-radius:100%; overflow:hidden; display:flex; justify-content:center; border:solid 3px #fcfcfc; box-shadow:0 1em 1em -1em rgba(0,0,0,.75); transition: all .5s; }
+
+section .image .inset { margin:auto; height:100%; width:100%; border-radius:100%; overflow:hidden; display:flex; justify-content:center; transition: all .5s; }
+
+section .image:hover { border-color:#069;  box-shadow:0 2em 2em -1em rgba(0,0,0,.55); transform:translate(0px,-1em); overflow:hidden; }
+
+section .image img { height:100%; display:block; margin:0 auto; transition:all 5s; }
+
+section .image:hover .inset { transform:scale(2); }
+
+section .description { border:solid 2px #eee; background:#fff; padding:1em 2em 2em; border-radius:.25em; box-shadow:0 1em 1em -1em rgba(0,0,0,.75); font-size:1.75em; transition:all 1s }
+section .description:hover { border:solid 2px #369; transform:translate(0px,-1em); }
+section .description:hover h1 { color:#369 }
+
+section.light { padding:6em 0; background:#ccc; }
+section.dark { padding:6em 0; background:#234; }
+
+.typed-cursor{
+    opacity: 1;
+    -webkit-animation: blink 0.7s infinite;
+    -moz-animation: blink 0.7s infinite;
+    animation: blink 0.7s infinite;
+}
+@keyframes blink{
+    0% { opacity:1; }
+    50% { opacity:0; }
+    100% { opacity:1; }
+}
+
+
+
+/* Make it a marquee */
+.quot-e {
+    margin: 0 auto;
+    overflow: hidden;
+    white-space: nowrap;
+    box-sizing: border-box;
+    animation: marquee 24s linear infinite;
+}
+
+.quote:hover {
+    animation-play-state: paused
+}
+
+/* Make it move */
+@keyframes marquee {
+    0%   { margin-left: 100% }
+    100% { margin-left: -100% }
+}
+
+</style>
